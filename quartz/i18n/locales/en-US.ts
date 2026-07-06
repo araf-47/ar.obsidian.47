ab60cb6b0a48803c0c48ac7b77ea5591a0794bba
Araf47.edu <araf47.edu@gmail.com>
2026-07-07 01:26:30 +0600

    Quartz sync: Jul 7, 2026, 1:26 AM

--- quartz/i18n/locales/en-US.ts
+++ quartz/i18n/locales/en-US.ts
@@ -36,7 +36,7 @@ export default {
       title: "Explorer",
     },
     footer: {
-      createdWith: "Created with",
+      createdWith: "Designed and built by",
     },
     graph: {
       title: "Graph View",
